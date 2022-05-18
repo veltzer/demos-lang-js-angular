c4a9c2da5e3abfb970b08e3fe7cf7381d3c11d7e
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:54 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-angular"
-project_long_description = "Demos for Angular"
-project_year_started = 2016
-project_description = """Demos for Angular"""
+description_short = "Demos for Angular"
